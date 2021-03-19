64199f986907c13a2131348ee20f450b4627396e
Sam Stephenson <sam@stephenson.net>
2021-03-19 15:30:53 +0000

    markers highlighting too

--- src/ClockMarkers.js
+++ src/ClockMarkers.js
@@ -1,5 +1,21 @@
-export default function Markers() {
+export default function Markers(props) {
   const markers = [];
+  const timeRounded = props.timeRounded;
+
+  const clockColoring = (i) => {
+    if (i === timeRounded) {
+      return "opacity-100";
+    } else if (i >= timeRounded - 1 && i <= timeRounded + 1) {
+      return "opacity-90";
+    } else if (i >= timeRounded - 2 && i <= timeRounded + 2) {
+      return "opacity-80";
+    } else if (i >= timeRounded - 3 && i <= timeRounded + 3) {
+      return "opacity-70";
+    } else {
+      return "opacity-60";
+    }
+  };
+
   for (var j = 0; j < 48; j++) {
     markers.push(
       <div
@@ -12,7 +28,7 @@ export default function Markers() {
         <div
           className={`w-0.5 ${
             j % 2 === 0 ? "h-1" : "h-2"
-          } bg-white opacity-50 rounded-full`}
+          } bg-white ${clockColoring(j / 2)} rounded-full`}
         />
       </div>
     );
